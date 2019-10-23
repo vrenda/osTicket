872506cbe89622e265bc8181ac346f6f2c105086
Vito Renda <vitorenda@gmail.com>
2019-10-23 08:55:18 +0200

    Init git repository

---
+++ ipclient.inc.php
@@ -0,0 +1,29 @@
+<?php
+/*********************************************************************
+    ipclient.inc.php
+
+    File included on index page
+
+    Vito Renda <vitorenda@gmail.com>
+    
+    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+    See LICENSE.TXT for details.
+
+**********************************************************************/
+function get_client_ip() {
+  return ($ip=getenv('HTTP_CLIENT_IP' )) ? $ip :
+	( ($ip=getenv('HTTP_X_FORWARDED_FOR' )) ? $ip :
+		( ($ip=getenv('HTTP_X_FORWARDED' )) ? $ip :
+			( ($ip=getenv('HTTP_FORWARDED_FOR' )) ? $ip :
+				( ($ip=getenv('HTTP_FORWARDED' )) ? $ip :
+					( ($ip=getenv('REMOTE_ADDR' )) ? $ip : false )
+				)
+			)
+		)
+	);
+}
+
+$clientIP = get_client_ip();
+if ($clientIP){ ?>
+	<h3 class="client-ip"> Il tuo IP: <?php echo $clientIP; ?></h3>
+<?php } ?>
